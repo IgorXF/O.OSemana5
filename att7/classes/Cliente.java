d7bfb1a28d56f4d2612ff42301354b83781a15e8
Igor Xisto <136636456+IgorXF@users.noreply.github.com>
2024-03-24 16:52:34 -0300

    Create Cliente.java

---
+++ att7/classes/Cliente.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+/**
+ *
+ * @author igorxf
+ */
+public class Cliente {
+    private String nome;
+
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
